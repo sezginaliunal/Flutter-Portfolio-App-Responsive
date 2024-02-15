70ff43e31e1096ef8912a9787f55247e998db532
Sezgin <sezginaliunal@hotmail.com>
2024-02-15 14:07:05 +0300

    Finished Project

---
+++ lib/screens/main/components/skills.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_app/components/animated_progress_indicator.dart';
+
+import '../../../constants.dart';
+
+class Skills extends StatelessWidget {
+  const Skills({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            "Skills",
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
+        const Row(
+          children: [
+            Expanded(
+              child: AnimatedCircularProgressIndicator(
+                percentage: 0.8,
+                label: "Flutter",
+              ),
+            ),
+            SizedBox(width: defaultPadding),
+            Expanded(
+              child: AnimatedCircularProgressIndicator(
+                percentage: 0.72,
+                label: "MongoDb",
+              ),
+            ),
+            SizedBox(width: defaultPadding),
+            Expanded(
+              child: AnimatedCircularProgressIndicator(
+                percentage: 0.65,
+                label: "Firebase",
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: defaultPadding),
+        const Row(
+          children: [
+            Expanded(
+              child: AnimatedCircularProgressIndicator(
+                percentage: 0.8,
+                label: "NodeJs",
+              ),
+            ),
+            SizedBox(width: defaultPadding),
+            Expanded(
+              child: AnimatedCircularProgressIndicator(
+                percentage: 0.72,
+                label: "SQL",
+              ),
+            ),
+            SizedBox(width: defaultPadding),
+            Expanded(
+              child: AnimatedCircularProgressIndicator(
+                percentage: 0.72,
+                label: "SQL",
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
